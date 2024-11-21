c0c7af6087ca7d5d23c915f7cda044347f23a24f
Harsh Surwase <160159587+RoyalTitan@users.noreply.github.com>
2024-11-21 16:19:16 +0000

    container config done

---
+++ 01_baiscs/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 85545
+let accountEmail = "harsh@gmail.com"
+let accountPassword = "789"
+accountCity = "Mumbai"
+accountName = 'harsh'
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountName])
