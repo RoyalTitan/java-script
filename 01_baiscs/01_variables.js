764d8ae56cae5e7e900027ddac71685b340cb7ed
Harsh Surwase <160159587+RoyalTitan@users.noreply.github.com>
2024-11-21 17:18:40 +0000

    variables

--- 01_baiscs/01_variables.js
+++ 01_baiscs/01_variables.js
@@ -3,5 +3,6 @@ let accountEmail = "harsh@gmail.com"
 let accountPassword = "789"
 accountCity = "Mumbai"
 accountName = 'harsh'
+accountName = "sanket"
 
 console.table([accountId, accountEmail, accountPassword, accountCity, accountName])
