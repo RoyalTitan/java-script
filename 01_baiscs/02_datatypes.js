7fb7e8ff59f522016a79474d5700153842843647
Harsh Surwase <160159587+RoyalTitan@users.noreply.github.com>
2024-11-21 17:13:35 +0000

    datatypes

---
+++ 01_baiscs/02_datatypes.js
@@ -0,0 +1,19 @@
+let name = "Harsh"
+let age = 21
+let isLoggedIn = false
+let state
+
+console.table([ name, age, isLoggedIn, state])
+
+// number => 2 to power 53
+// BigInt
+// String => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+console.log(typeof undefined); //undefiend
+console.log(typeof null); // object
+
